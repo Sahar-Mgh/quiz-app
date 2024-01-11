5a6af238085af9b27ec6677737690371ae437670
Sahar-Mgh <sahar.emma.m@gmail.com>
2024-01-11 19:03:57 +0330

    added scoring function

--- script.js
+++ script.js
@@ -95,6 +95,13 @@ function selectAnswer(e){
     nextButton.style.display = "block";
 }
 
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
 function handleNextButton(){
     currentQuestionIndex++;
     if(currentQuestionIndex < questions.length){
